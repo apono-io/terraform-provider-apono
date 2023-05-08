6d9385effbf1afb4da273e6eda4dc7f01701afa1
Asaf Alima <asaf.alima@gmail.com>
2023-05-08 13:08:44 +0300

    Add resource for integrations

---
+++ internal/provider/integration_resource.go
@@ -0,0 +1,497 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"github.com/apono-io/apono-sdk-go"
+	"github.com/hashicorp/terraform-plugin-framework-validators/helpers/validatordiag"
+	"github.com/hashicorp/terraform-plugin-framework-validators/resourcevalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"golang.org/x/exp/slices"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &IntegrationResource{}
+var _ resource.ResourceWithImportState = &IntegrationResource{}
+var _ resource.ResourceWithValidateConfig = &IntegrationResource{}
+
+var (
+	secretTypeAttributeNames = map[string]string{
+		"AWS":        "aws_secret",
+		"GCP":        "gcp_secret",
+		"KUBERNETES": "kubernetes_secret",
+	}
+
+	awsSecretAttrMap = map[string]attr.Type{
+		"region":     basetypes.StringType{},
+		"secret_arn": basetypes.StringType{},
+	}
+	gcpSecretAttrMap = map[string]attr.Type{
+		"project":   basetypes.StringType{},
+		"secret_id": basetypes.StringType{},
+	}
+	kubernetesSecretAttrMap = map[string]attr.Type{
+		"namespace": basetypes.StringType{},
+		"name":      basetypes.StringType{},
+	}
+)
+
+func NewIntegrationResource() resource.Resource {
+	return &IntegrationResource{}
+}
+
+// IntegrationResource defines the resource implementation.
+type IntegrationResource struct {
+	provider *AponoProvider
+}
+
+// IntegrationResourceModel describes the resource data model.
+type IntegrationResourceModel struct {
+	ID               types.String `tfsdk:"id"`
+	Name             types.String `tfsdk:"name"`
+	Type             types.String `tfsdk:"type"`
+	ConnectorID      types.String `tfsdk:"connector_id"`
+	Metadata         types.Map    `tfsdk:"metadata"`
+	AwsSecret        types.Object `tfsdk:"aws_secret"`
+	GcpSecret        types.Object `tfsdk:"gcp_secret"`
+	KubernetesSecret types.Object `tfsdk:"kubernetes_secret"`
+}
+
+func (r *IntegrationResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_integration"
+}
+
+func (r *IntegrationResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "Example resource",
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Example identifier",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"name": schema.StringAttribute{
+				MarkdownDescription: "Example configurable attribute",
+				Required:            true,
+			},
+			"type": schema.StringAttribute{
+				MarkdownDescription: "Example configurable attribute with default value",
+				Required:            true,
+			},
+			"connector_id": schema.StringAttribute{
+				MarkdownDescription: "Example configurable attribute with default value",
+				Required:            true,
+			},
+			"metadata": schema.MapAttribute{
+				MarkdownDescription: "Example configurable attribute with default value",
+				Optional:            true,
+				ElementType:         types.StringType,
+			},
+			"aws_secret": schema.SingleNestedAttribute{
+				Optional: true,
+				Attributes: map[string]schema.Attribute{
+					"region": schema.StringAttribute{
+						MarkdownDescription: "Example configurable attribute",
+						Required:            true,
+					},
+					"secret_arn": schema.StringAttribute{
+						MarkdownDescription: "Example configurable attribute",
+						Required:            true,
+					},
+				},
+			},
+			"gcp_secret": schema.SingleNestedAttribute{
+				Optional: true,
+				Attributes: map[string]schema.Attribute{
+					"project": schema.StringAttribute{
+						MarkdownDescription: "Example configurable attribute",
+						Required:            true,
+					},
+					"secret_id": schema.StringAttribute{
+						MarkdownDescription: "Example configurable attribute",
+						Required:            true,
+					},
+				},
+			},
+			"kubernetes_secret": schema.SingleNestedAttribute{
+				Optional: true,
+				Attributes: map[string]schema.Attribute{
+					"namespace": schema.StringAttribute{
+						MarkdownDescription: "Example configurable attribute",
+						Required:            true,
+					},
+					"name": schema.StringAttribute{
+						MarkdownDescription: "Example configurable attribute",
+						Required:            true,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (r *IntegrationResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	r.provider, resp.Diagnostics = toProvider(req.ProviderData)
+}
+
+func (r *IntegrationResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data *IntegrationResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	metadata := map[string]interface{}{}
+	for name, value := range data.Metadata.Elements() {
+		metadata[name] = attrValueToString(value)
+	}
+
+	var secretConfig map[string]interface{}
+	if !data.AwsSecret.IsNull() {
+		attributes := data.AwsSecret.Attributes()
+		secretConfig = map[string]interface{}{
+			"type":      "AWS",
+			"region":    attrValueToString(attributes["region"]),
+			"secret_id": attrValueToString(attributes["secret_arn"]),
+		}
+	} else if !data.GcpSecret.IsNull() {
+		attributes := data.GcpSecret.Attributes()
+		secretConfig = map[string]interface{}{
+			"type":      "GCP",
+			"project":   attrValueToString(attributes["project"]),
+			"secret_id": attrValueToString(attributes["secret_id"]),
+		}
+	} else if !data.KubernetesSecret.IsNull() {
+		attributes := data.KubernetesSecret.Attributes()
+		secretConfig = map[string]interface{}{
+			"type":      "KUBERNETES",
+			"namespace": attrValueToString(attributes["namespace"]),
+			"name":      attrValueToString(attributes["name"]),
+		}
+	}
+
+	connectorID := data.ConnectorID.ValueString()
+	integration, _, err := r.provider.client.IntegrationsApi.CreateIntegrationV2(ctx).
+		CreateIntegration(apono.CreateIntegration{
+			Name:          data.Name.ValueString(),
+			Type:          data.Type.ValueString(),
+			ProvisionerId: *apono.NewNullableString(&connectorID),
+			Metadata:      metadata,
+			SecretConfig:  secretConfig,
+		}).
+		Execute()
+	if err != nil {
+		if apiError, ok := err.(*apono.GenericOpenAPIError); ok {
+			resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to create integration, error: %s, body: %s", apiError.Error(), string(apiError.Body())))
+		} else {
+			resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to create integration: %s", err.Error()))
+		}
+
+		return
+	}
+
+	model, diagnostics := r.convertToModel(ctx, integration)
+	if len(diagnostics) > 0 {
+		resp.Diagnostics.Append(diagnostics...)
+		return
+	}
+
+	tflog.Trace(ctx, "created integration", map[string]interface{}{
+		"id": data.ID.ValueString(),
+	})
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &model)...)
+}
+
+func (r *IntegrationResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data *IntegrationResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read example, got error: %s", err))
+	//     return
+	// }
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *IntegrationResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data *IntegrationResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	metadata := map[string]interface{}{}
+	for name, value := range data.Metadata.Elements() {
+		metadata[name] = attrValueToString(value)
+	}
+
+	var secretConfig map[string]interface{}
+	if !data.AwsSecret.IsNull() {
+		attributes := data.AwsSecret.Attributes()
+		secretConfig = map[string]interface{}{
+			"type":      "AWS",
+			"region":    attrValueToString(attributes["region"]),
+			"secret_id": attrValueToString(attributes["secret_arn"]),
+		}
+	} else if !data.GcpSecret.IsNull() {
+		attributes := data.GcpSecret.Attributes()
+		secretConfig = map[string]interface{}{
+			"type":      "GCP",
+			"project":   attrValueToString(attributes["project"]),
+			"secret_id": attrValueToString(attributes["secret_id"]),
+		}
+	} else if !data.KubernetesSecret.IsNull() {
+		attributes := data.KubernetesSecret.Attributes()
+		secretConfig = map[string]interface{}{
+			"type":      "KUBERNETES",
+			"namespace": attrValueToString(attributes["namespace"]),
+			"name":      attrValueToString(attributes["name"]),
+		}
+	}
+
+	connectorID := data.ConnectorID.ValueString()
+	integration, _, err := r.provider.client.IntegrationsApi.UpdateIntegrationV2(ctx, data.ID.ValueString()).
+		UpdateIntegration(apono.UpdateIntegration{
+			Name:          data.Name.ValueString(),
+			ProvisionerId: *apono.NewNullableString(&connectorID),
+			Metadata:      metadata,
+			SecretConfig:  secretConfig,
+		}).
+		Execute()
+	if err != nil {
+		if apiError, ok := err.(*apono.GenericOpenAPIError); ok {
+			resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to update integration with id %s, error: %s, body: %s", data.ID, apiError.Error(), string(apiError.Body())))
+		} else {
+			resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to update integration with id %s: %s", data.ID, err.Error()))
+		}
+
+		return
+	}
+
+	model, diagnostics := r.convertToModel(ctx, integration)
+	if len(diagnostics) > 0 {
+		resp.Diagnostics.Append(diagnostics...)
+		return
+	}
+
+	tflog.Trace(ctx, "updated integration", map[string]interface{}{
+		"id": data.ID.ValueString(),
+	})
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &model)...)
+}
+
+func (r *IntegrationResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data *IntegrationResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	messageResponse, _, err := r.provider.client.IntegrationsApi.DeleteIntegrationV2(ctx, data.ID.ValueString()).
+		Execute()
+	if err != nil {
+		if apiError, ok := err.(*apono.GenericOpenAPIError); ok {
+			resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to delete integration with id %s, error: %s, body: %s", data.ID, apiError.Error(), string(apiError.Body())))
+		} else {
+			resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to delete integration with id %s: %s", data.ID, err.Error()))
+		}
+
+		return
+	}
+
+	tflog.Debug(ctx, "deleted integration", map[string]interface{}{
+		"id":       data.ID.ValueString(),
+		"response": messageResponse.GetMessage(),
+	})
+}
+
+func (r *IntegrationResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	integrationId := req.ID
+	tflog.Debug(ctx, "importing integration", map[string]interface{}{
+		"id": integrationId,
+	})
+
+	integration, _, err := r.provider.client.IntegrationsApi.GetIntegrationV2(ctx, integrationId).
+		Execute()
+	if err != nil {
+		if apiError, ok := err.(*apono.GenericOpenAPIError); ok {
+			resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to get integration with id %s, error: %s, body: %s", integrationId, apiError.Error(), string(apiError.Body())))
+		} else {
+			resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to get integration with id %s: %s", integrationId, err.Error()))
+		}
+
+		return
+	}
+
+	model, diagnostics := r.convertToModel(ctx, integration)
+	if len(diagnostics) > 0 {
+		resp.Diagnostics.Append(diagnostics...)
+		return
+	}
+
+	// Save imported data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &model)...)
+
+	tflog.Debug(ctx, "imported integration", map[string]interface{}{
+		"id": integrationId,
+	})
+}
+
+func (r *IntegrationResource) ValidateConfig(ctx context.Context, req resource.ValidateConfigRequest, resp *resource.ValidateConfigResponse) {
+	if r.provider == nil {
+		return
+	}
+
+	var model IntegrationResourceModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &model)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	config, _, err := r.provider.client.IntegrationsApi.GetIntegrationConfig(ctx, model.Type.ValueString()).
+		Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to get config: %s", err.Error()))
+		return
+	}
+
+	if config.GetRequiresSecret() {
+		var supportedSecretExpressions []path.Expression
+		for _, secretType := range config.SupportedSecretTypes {
+			if attributeName, ok := secretTypeAttributeNames[secretType]; ok {
+				supportedSecretExpressions = append(supportedSecretExpressions, path.MatchRoot(attributeName))
+			}
+		}
+
+		resourcevalidator.ExactlyOneOf(supportedSecretExpressions...).ValidateResource(ctx, req, resp)
+	}
+
+	metadataElements := model.Metadata.Elements()
+	for _, param := range config.GetParams() {
+		paramName := param.GetId()
+		paramPossibleValues := param.GetValues()
+		paramDefaultValue := param.GetDefault()
+
+		attributePath := path.Root(fmt.Sprintf(`metadata["%s"]`, paramName))
+
+		metadataValue, hasValue := metadataElements[paramName]
+		if !hasValue {
+			if paramDefaultValue != "" {
+				resp.Diagnostics.AddAttributeError(
+					attributePath,
+					"Missing Configuration for Required Attribute",
+					fmt.Sprintf("Must set a configuration value for the %s attribute as the provider has marked it as required.\n\n", attributePath.String())+
+						"Refer to the provider documentation or contact the provider developers for additional information about configurable attributes that are required.\n\n"+
+						fmt.Sprintf("Configuring this integration through the UI will use default value: %s", paramDefaultValue),
+				)
+			} else {
+				resp.Diagnostics.AddAttributeError(
+					attributePath,
+					"Missing Configuration for Required Attribute",
+					fmt.Sprintf("Must set a configuration value for the %s attribute as the provider has marked it as required.\n\n", attributePath.String())+
+						"Refer to the provider documentation or contact the provider developers for additional information about configurable attributes that are required.",
+				)
+			}
+
+			continue
+		}
+
+		metadataValueStr := attrValueToString(metadataValue)
+		if len(paramPossibleValues) > 0 && !slices.Contains(paramPossibleValues, metadataValueStr) {
+			resp.Diagnostics.Append(validatordiag.InvalidAttributeValueMatchDiagnostic(
+				attributePath,
+				fmt.Sprintf("value must be one of: %q", paramPossibleValues),
+				metadataValueStr,
+			))
+		}
+	}
+}
+
+func (r *IntegrationResource) convertToModel(ctx context.Context, integration *apono.Integration) (*IntegrationResourceModel, diag.Diagnostics) {
+	metadataMapValue, diagnostics := types.MapValueFrom(ctx, types.StringType, integration.GetMetadata())
+
+	data := IntegrationResourceModel{}
+	data.ID = types.StringValue(integration.GetId())
+	data.Name = types.StringValue(integration.GetName())
+	data.Type = types.StringValue(integration.GetType())
+	data.ConnectorID = types.StringValue(integration.GetProvisionerId())
+	data.Metadata = metadataMapValue
+
+	data.AwsSecret = types.ObjectNull(awsSecretAttrMap)
+	data.GcpSecret = types.ObjectNull(gcpSecretAttrMap)
+	data.KubernetesSecret = types.ObjectNull(kubernetesSecretAttrMap)
+
+	secretConfig := integration.GetSecretConfig()
+	switch secretConfig["type"] {
+	case "AWS":
+		secretAttributes := map[string]attr.Value{
+			"region":     basetypes.NewStringValue(fmt.Sprintf("%v", secretConfig["region"])),
+			"secret_arn": basetypes.NewStringValue(fmt.Sprintf("%v", secretConfig["secret_id"])),
+		}
+		data.AwsSecret, diagnostics = types.ObjectValue(awsSecretAttrMap, secretAttributes)
+	case "GCP":
+		secretAttributes := map[string]attr.Value{
+			"project":   basetypes.NewStringValue(fmt.Sprintf("%v", secretConfig["project"])),
+			"secret_id": basetypes.NewStringValue(fmt.Sprintf("%v", secretConfig["secret_id"])),
+		}
+		data.GcpSecret, diagnostics = types.ObjectValue(gcpSecretAttrMap, secretAttributes)
+	case "KUBERNETES":
+		secretAttributes := map[string]attr.Value{
+			"namespace": basetypes.NewStringValue(fmt.Sprintf("%v", secretConfig["namespace"])),
+			"name":      basetypes.NewStringValue(fmt.Sprintf("%v", secretConfig["name"])),
+		}
+		data.KubernetesSecret, diagnostics = types.ObjectValue(kubernetesSecretAttrMap, secretAttributes)
+	}
+
+	if len(diagnostics) > 0 {
+		return nil, diagnostics
+	}
+
+	return &data, nil
+}
+
+func attrValueToString(val attr.Value) string {
+	switch value := val.(type) {
+	case types.String:
+		return value.ValueString()
+	default:
+		return value.String()
+	}
+}
---
+++ internal/provider/integration_resource_test.go
@@ -0,0 +1,206 @@
+package provider
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/apono-io/apono-sdk-go"
+	"github.com/google/uuid"
+	"github.com/jarcoal/httpmock"
+	"net/http"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccIntegrationResource(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	var integrations = map[string]*apono.Integration{}
+	httpmock.RegisterResponder(http.MethodPost, "http://api.apono.dev/api/v2/integrations", func(req *http.Request) (*http.Response, error) {
+		var createReq apono.CreateIntegration
+		if err := json.NewDecoder(req.Body).Decode(&createReq); err != nil {
+			return httpmock.NewStringResponse(400, err.Error()), nil
+		}
+
+		id, err := uuid.NewUUID()
+		if err != nil {
+			return httpmock.NewStringResponse(500, err.Error()), nil
+		}
+
+		integration := apono.Integration{
+			Id:            id.String(),
+			Name:          createReq.Name,
+			Type:          createReq.Type,
+			ProvisionerId: createReq.ProvisionerId,
+			Status:        apono.INTEGRATIONSTATUS_ACTIVE,
+			Metadata:      createReq.Metadata,
+			SecretConfig:  createReq.SecretConfig,
+		}
+		integrations[integration.Id] = &integration
+
+		resp, err := httpmock.NewJsonResponse(200, integration)
+		if err != nil {
+			return httpmock.NewStringResponse(500, err.Error()), nil
+		}
+
+		return resp, nil
+	})
+
+	httpmock.RegisterResponder(http.MethodGet, `=~^http://api\.apono\.dev/api/v2/integrations/([^/]+)\z`, func(req *http.Request) (*http.Response, error) {
+		id := httpmock.MustGetSubmatch(req, 1) // 1=first regexp submatch
+		integration, exists := integrations[id]
+		if !exists {
+			return httpmock.NewStringResponse(404, "Integration not found"), nil
+		}
+
+		resp, err := httpmock.NewJsonResponse(200, integration)
+		if err != nil {
+			return httpmock.NewStringResponse(500, err.Error()), nil
+		}
+
+		return resp, nil
+	})
+
+	httpmock.RegisterResponder(http.MethodPut, `=~^http://api\.apono\.dev/api/v2/integrations/([^/]+)\z`, func(req *http.Request) (*http.Response, error) {
+		id := httpmock.MustGetSubmatch(req, 1) // 1=first regexp submatch
+
+		var updateReq apono.UpdateIntegration
+		if err := json.NewDecoder(req.Body).Decode(&updateReq); err != nil {
+			return httpmock.NewStringResponse(400, err.Error()), nil
+		}
+
+		integration, exists := integrations[id]
+		if !exists {
+			return httpmock.NewStringResponse(404, "Integration not found"), nil
+		}
+
+		integration.Name = updateReq.Name
+		integration.ProvisionerId = updateReq.ProvisionerId
+		integration.Metadata = updateReq.Metadata
+		integration.SecretConfig = updateReq.SecretConfig
+
+		resp, err := httpmock.NewJsonResponse(200, integration)
+		if err != nil {
+			return httpmock.NewStringResponse(500, err.Error()), nil
+		}
+
+		return resp, nil
+	})
+
+	httpmock.RegisterResponder(http.MethodDelete, `=~^http://api\.apono\.dev/api/v2/integrations/([^/]+)\z`, func(req *http.Request) (*http.Response, error) {
+		id := httpmock.MustGetSubmatch(req, 1) // 1=first regexp submatch
+
+		delete(integrations, id)
+
+		messageResponse := apono.MessageResponse{
+			Message: "Deleted integration",
+		}
+
+		resp, err := httpmock.NewJsonResponse(200, messageResponse)
+		if err != nil {
+			return httpmock.NewStringResponse(500, err.Error()), nil
+		}
+
+		return resp, nil
+	})
+
+	httpmock.RegisterResponder(http.MethodGet, `=~^http://api\.apono\.dev/api/v2/integrations-catalog/([^/]+)\z`, func(req *http.Request) (*http.Response, error) {
+		configType := httpmock.MustGetSubmatch(req, 1) // 1=first regexp submatch
+		switch configType {
+		case "postgresql":
+			config := apono.IntegrationConfig{
+				Name:        "PostgreSQL",
+				Type:        "postgresql",
+				Description: "An open-source relational database management system emphasizing extensibility and SQL compliance.",
+				Params: []apono.IntegrationConfigParam{
+					{
+						Id:    "hostname",
+						Label: "Hostname",
+					},
+					{
+						Id:      "port",
+						Label:   "Port",
+						Default: "5432",
+					},
+					{
+						Id:      "dbname",
+						Label:   "Database Name",
+						Default: "postgres",
+					},
+				},
+				RequiresSecret: true,
+				SupportedSecretTypes: []string{
+					"AWS",
+					"GCP",
+					"KUBERNETES",
+				},
+			}
+
+			resp, err := httpmock.NewJsonResponse(200, config)
+			if err != nil {
+				return httpmock.NewStringResponse(500, err.Error()), nil
+			}
+
+			return resp, nil
+		default:
+			return httpmock.NewStringResponse(400, "Unsupported config type"), nil
+		}
+	})
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read
+			{
+				Config: testAccExampleResourceConfig("one"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("apono_integration.test", "id"),
+					resource.TestCheckResourceAttr("apono_integration.test", "name", "one"),
+					resource.TestCheckResourceAttr("apono_integration.test", "type", "postgresql"),
+					resource.TestCheckResourceAttr("apono_integration.test", "aws_secret.region", "us-east-1"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "apono_integration.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Update and Read testing
+			{
+				Config: testAccExampleResourceConfig("two"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("apono_integration.test", "name", "two"),
+					resource.TestCheckResourceAttr("apono_integration.test", "type", "postgresql"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccExampleResourceConfig(configurableAttribute string) string {
+	return fmt.Sprintf(`
+provider apono {
+  endpoint = "http://api.apono.dev"
+  personal_token = "1234567890abcdefg"
+}
+
+resource "apono_integration" "test" {
+  name = "%[1]s"
+  type = "postgresql"
+  connector_id = "000-1111-222222-33333-444444"
+  metadata = {
+    hostname = "my-postgres-rds.aaabbbsss111.us-east-1.rds.amazonaws.com"
+    port = "5432"
+    dbname = "postgres"
+  }
+  aws_secret = {
+    region = "us-east-1"
+    secret_arn = "my-secret"
+  }
+}
+`, configurableAttribute)
+}
