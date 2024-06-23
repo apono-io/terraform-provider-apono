0cec265340a9577e213bd49e387d2bb40102b3f5
Tomer Stein <tomer.s@apono.io>
2024-06-23 18:28:04 +0300

    update terraform api

--- internal/aponoapi/model_upsert_integration_terraform.go
+++ internal/aponoapi/model_upsert_integration_terraform.go
@@ -24,11 +24,11 @@ type UpsertIntegrationTerraform struct {
 	Type                   string                          `json:"type"`
 	ProvisionerId          NullableString                  `json:"provisioner_id,omitempty"`
 	Params                 map[string]interface{}          `json:"params"`
-	SecretConfig           map[string]interface{}          `json:"secret_config"`
-	ConnectedResourceTypes []string                        `json:"connected_resource_types"`
-	CustomAccessDetails    string                          `json:"custom_access_details"`
-	IntegrationOwners      IntegrationOwnersTerraform      `json:"integration_owners"`
-	ResourceOwnersMappings []ResourceOwnerMappingTerraform `json:"resource_owners_mappings"`
+	SecretConfig           map[string]interface{}          `json:"secret_config,omitempty"`
+	ConnectedResourceTypes []string                        `json:"connected_resource_types,omitempty"`
+	CustomAccessDetails    NullableString                  `json:"custom_access_details,omitempty"`
+	IntegrationOwners      []IntegrationOwnerTerraform     `json:"integration_owners,omitempty"`
+	ResourceOwnersMappings []ResourceOwnerMappingTerraform `json:"resource_owners_mappings,omitempty"`
 }
 
 type _UpsertIntegrationTerraform UpsertIntegrationTerraform
@@ -37,16 +37,11 @@ type _UpsertIntegrationTerraform UpsertIntegrationTerraform
 // This constructor will assign default values to properties that have it defined,
 // and makes sure properties required by API are set, but the set of arguments
 // will change when the set of required properties is changed
-func NewUpsertIntegrationTerraform(name string, type_ string, params map[string]interface{}, secretConfig map[string]interface{}, connectedResourceTypes []string, customAccessDetails string, integrationOwners IntegrationOwnersTerraform, resourceOwnersMappings []ResourceOwnerMappingTerraform) *UpsertIntegrationTerraform {
+func NewUpsertIntegrationTerraform(name string, type_ string, params map[string]interface{}) *UpsertIntegrationTerraform {
 	this := UpsertIntegrationTerraform{}
 	this.Name = name
 	this.Type = type_
 	this.Params = params
-	this.SecretConfig = secretConfig
-	this.ConnectedResourceTypes = connectedResourceTypes
-	this.CustomAccessDetails = customAccessDetails
-	this.IntegrationOwners = integrationOwners
-	this.ResourceOwnersMappings = resourceOwnersMappings
 	return &this
 }
 
@@ -173,122 +168,177 @@ func (o *UpsertIntegrationTerraform) SetParams(v map[string]interface{}) {
 	o.Params = v
 }
 
-// GetSecretConfig returns the SecretConfig field value
+// GetSecretConfig returns the SecretConfig field value if set, zero value otherwise (both if not set or set to explicit null).
 func (o *UpsertIntegrationTerraform) GetSecretConfig() map[string]interface{} {
 	if o == nil {
 		var ret map[string]interface{}
 		return ret
 	}
-
 	return o.SecretConfig
 }
 
-// GetSecretConfigOk returns a tuple with the SecretConfig field value
+// GetSecretConfigOk returns a tuple with the SecretConfig field value if set, nil otherwise
 // and a boolean to check if the value has been set.
+// NOTE: If the value is an explicit nil, `nil, true` will be returned
 func (o *UpsertIntegrationTerraform) GetSecretConfigOk() (map[string]interface{}, bool) {
-	if o == nil {
+	if o == nil || IsNil(o.SecretConfig) {
 		return map[string]interface{}{}, false
 	}
 	return o.SecretConfig, true
 }
 
-// SetSecretConfig sets field value
+// HasSecretConfig returns a boolean if a field has been set.
+func (o *UpsertIntegrationTerraform) HasSecretConfig() bool {
+	if o != nil && IsNil(o.SecretConfig) {
+		return true
+	}
+
+	return false
+}
+
+// SetSecretConfig gets a reference to the given map[string]interface{} and assigns it to the SecretConfig field.
 func (o *UpsertIntegrationTerraform) SetSecretConfig(v map[string]interface{}) {
 	o.SecretConfig = v
 }
 
-// GetConnectedResourceTypes returns the ConnectedResourceTypes field value
+// GetConnectedResourceTypes returns the ConnectedResourceTypes field value if set, zero value otherwise (both if not set or set to explicit null).
 func (o *UpsertIntegrationTerraform) GetConnectedResourceTypes() []string {
 	if o == nil {
 		var ret []string
 		return ret
 	}
-
 	return o.ConnectedResourceTypes
 }
 
-// GetConnectedResourceTypesOk returns a tuple with the ConnectedResourceTypes field value
+// GetConnectedResourceTypesOk returns a tuple with the ConnectedResourceTypes field value if set, nil otherwise
 // and a boolean to check if the value has been set.
+// NOTE: If the value is an explicit nil, `nil, true` will be returned
 func (o *UpsertIntegrationTerraform) GetConnectedResourceTypesOk() ([]string, bool) {
-	if o == nil {
+	if o == nil || IsNil(o.ConnectedResourceTypes) {
 		return nil, false
 	}
 	return o.ConnectedResourceTypes, true
 }
 
-// SetConnectedResourceTypes sets field value
+// HasConnectedResourceTypes returns a boolean if a field has been set.
+func (o *UpsertIntegrationTerraform) HasConnectedResourceTypes() bool {
+	if o != nil && IsNil(o.ConnectedResourceTypes) {
+		return true
+	}
+
+	return false
+}
+
+// SetConnectedResourceTypes gets a reference to the given []string and assigns it to the ConnectedResourceTypes field.
 func (o *UpsertIntegrationTerraform) SetConnectedResourceTypes(v []string) {
 	o.ConnectedResourceTypes = v
 }
 
-// GetCustomAccessDetails returns the CustomAccessDetails field value
+// GetCustomAccessDetails returns the CustomAccessDetails field value if set, zero value otherwise (both if not set or set to explicit null).
 func (o *UpsertIntegrationTerraform) GetCustomAccessDetails() string {
-	if o == nil {
+	if o == nil || IsNil(o.CustomAccessDetails.Get()) {
 		var ret string
 		return ret
 	}
-
-	return o.CustomAccessDetails
+	return *o.CustomAccessDetails.Get()
 }
 
-// GetCustomAccessDetailsOk returns a tuple with the CustomAccessDetails field value
+// GetCustomAccessDetailsOk returns a tuple with the CustomAccessDetails field value if set, nil otherwise
 // and a boolean to check if the value has been set.
+// NOTE: If the value is an explicit nil, `nil, true` will be returned
 func (o *UpsertIntegrationTerraform) GetCustomAccessDetailsOk() (*string, bool) {
 	if o == nil {
 		return nil, false
 	}
-	return &o.CustomAccessDetails, true
+	return o.CustomAccessDetails.Get(), o.CustomAccessDetails.IsSet()
 }
 
-// SetCustomAccessDetails sets field value
-func (o *UpsertIntegrationTerraform) SetCustomAccessDetails(v string) {
-	o.CustomAccessDetails = v
-}
-
-// GetIntegrationOwners returns the IntegrationOwners field value
-func (o *UpsertIntegrationTerraform) GetIntegrationOwners() IntegrationOwnersTerraform {
-	if o == nil {
-		var ret IntegrationOwnersTerraform
-		return ret
+// HasCustomAccessDetails returns a boolean if a field has been set.
+func (o *UpsertIntegrationTerraform) HasCustomAccessDetails() bool {
+	if o != nil && o.CustomAccessDetails.IsSet() {
+		return true
 	}
 
+	return false
+}
+
+// SetCustomAccessDetails gets a reference to the given NullableString and assigns it to the CustomAccessDetails field.
+func (o *UpsertIntegrationTerraform) SetCustomAccessDetails(v string) {
+	o.CustomAccessDetails.Set(&v)
+}
+
+// SetCustomAccessDetailsNil sets the value for CustomAccessDetails to be an explicit nil
+func (o *UpsertIntegrationTerraform) SetCustomAccessDetailsNil() {
+	o.CustomAccessDetails.Set(nil)
+}
+
+// UnsetCustomAccessDetails ensures that no value is present for CustomAccessDetails, not even an explicit nil
+func (o *UpsertIntegrationTerraform) UnsetCustomAccessDetails() {
+	o.CustomAccessDetails.Unset()
+}
+
+// GetIntegrationOwners returns the IntegrationOwners field value if set, zero value otherwise (both if not set or set to explicit null).
+func (o *UpsertIntegrationTerraform) GetIntegrationOwners() []IntegrationOwnerTerraform {
+	if o == nil {
+		var ret []IntegrationOwnerTerraform
+		return ret
+	}
 	return o.IntegrationOwners
 }
 
-// GetIntegrationOwnersOk returns a tuple with the IntegrationOwners field value
+// GetIntegrationOwnersOk returns a tuple with the IntegrationOwners field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *UpsertIntegrationTerraform) GetIntegrationOwnersOk() (*IntegrationOwnersTerraform, bool) {
-	if o == nil {
+// NOTE: If the value is an explicit nil, `nil, true` will be returned
+func (o *UpsertIntegrationTerraform) GetIntegrationOwnersOk() ([]IntegrationOwnerTerraform, bool) {
+	if o == nil || IsNil(o.IntegrationOwners) {
 		return nil, false
 	}
-	return &o.IntegrationOwners, true
+	return o.IntegrationOwners, true
 }
 
-// SetIntegrationOwners sets field value
-func (o *UpsertIntegrationTerraform) SetIntegrationOwners(v IntegrationOwnersTerraform) {
+// HasIntegrationOwners returns a boolean if a field has been set.
+func (o *UpsertIntegrationTerraform) HasIntegrationOwners() bool {
+	if o != nil && IsNil(o.IntegrationOwners) {
+		return true
+	}
+
+	return false
+}
+
+// SetIntegrationOwners gets a reference to the given []IntegrationOwnerTerraform and assigns it to the IntegrationOwners field.
+func (o *UpsertIntegrationTerraform) SetIntegrationOwners(v []IntegrationOwnerTerraform) {
 	o.IntegrationOwners = v
 }
 
-// GetResourceOwnersMappings returns the ResourceOwnersMappings field value
+// GetResourceOwnersMappings returns the ResourceOwnersMappings field value if set, zero value otherwise (both if not set or set to explicit null).
 func (o *UpsertIntegrationTerraform) GetResourceOwnersMappings() []ResourceOwnerMappingTerraform {
 	if o == nil {
 		var ret []ResourceOwnerMappingTerraform
 		return ret
 	}
-
 	return o.ResourceOwnersMappings
 }
 
-// GetResourceOwnersMappingsOk returns a tuple with the ResourceOwnersMappings field value
+// GetResourceOwnersMappingsOk returns a tuple with the ResourceOwnersMappings field value if set, nil otherwise
 // and a boolean to check if the value has been set.
+// NOTE: If the value is an explicit nil, `nil, true` will be returned
 func (o *UpsertIntegrationTerraform) GetResourceOwnersMappingsOk() ([]ResourceOwnerMappingTerraform, bool) {
-	if o == nil {
+	if o == nil || IsNil(o.ResourceOwnersMappings) {
 		return nil, false
 	}
 	return o.ResourceOwnersMappings, true
 }
 
-// SetResourceOwnersMappings sets field value
+// HasResourceOwnersMappings returns a boolean if a field has been set.
+func (o *UpsertIntegrationTerraform) HasResourceOwnersMappings() bool {
+	if o != nil && IsNil(o.ResourceOwnersMappings) {
+		return true
+	}
+
+	return false
+}
+
+// SetResourceOwnersMappings gets a reference to the given []ResourceOwnerMappingTerraform and assigns it to the ResourceOwnersMappings field.
 func (o *UpsertIntegrationTerraform) SetResourceOwnersMappings(v []ResourceOwnerMappingTerraform) {
 	o.ResourceOwnersMappings = v
 }
@@ -309,11 +359,21 @@ func (o UpsertIntegrationTerraform) ToMap() (map[string]interface{}, error) {
 		toSerialize["provisioner_id"] = o.ProvisionerId.Get()
 	}
 	toSerialize["params"] = o.Params
-	toSerialize["secret_config"] = o.SecretConfig
-	toSerialize["connected_resource_types"] = o.ConnectedResourceTypes
-	toSerialize["custom_access_details"] = o.CustomAccessDetails
-	toSerialize["integration_owners"] = o.IntegrationOwners
-	toSerialize["resource_owners_mappings"] = o.ResourceOwnersMappings
+	if o.SecretConfig != nil {
+		toSerialize["secret_config"] = o.SecretConfig
+	}
+	if o.ConnectedResourceTypes != nil {
+		toSerialize["connected_resource_types"] = o.ConnectedResourceTypes
+	}
+	if o.CustomAccessDetails.IsSet() {
+		toSerialize["custom_access_details"] = o.CustomAccessDetails.Get()
+	}
+	if o.IntegrationOwners != nil {
+		toSerialize["integration_owners"] = o.IntegrationOwners
+	}
+	if o.ResourceOwnersMappings != nil {
+		toSerialize["resource_owners_mappings"] = o.ResourceOwnersMappings
+	}
 	return toSerialize, nil
 }
 
@@ -325,11 +385,6 @@ func (o *UpsertIntegrationTerraform) UnmarshalJSON(bytes []byte) (err error) {
 		"name",
 		"type",
 		"params",
-		"secret_config",
-		"connected_resource_types",
-		"custom_access_details",
-		"integration_owners",
-		"resource_owners_mappings",
 	}
 
 	allProperties := make(map[string]interface{})
