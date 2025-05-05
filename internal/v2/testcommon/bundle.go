6c263d3dc77aedfaead39c7e0290a31d17cb2444
Tomer Heber <heber.tomer@gmail.com>
2025-05-04 20:06:08 -0500

    fix linter issue

--- internal/v2/testcommon/bundle.go
+++ internal/v2/testcommon/bundle.go
@@ -4,14 +4,12 @@ import (
 	"github.com/apono-io/terraform-provider-apono/internal/v2/api/client"
 )
 
-// GenerateBundleResponse creates a mock bundle response for testing
 func GenerateBundleResponse() *client.BundlePublicV2Model {
 	bundle := &client.BundlePublicV2Model{
 		ID:   "bundle-123",
 		Name: "Test Bundle",
 	}
 
-	// Add an integration target
 	integrationTarget := client.AccessBundleAccessTargetPublicV2Model{}
 	integrationData := client.IntegrationAccessTargetPublicV2Model{
 		IntegrationID:   "integration-123",
@@ -27,7 +25,6 @@ func GenerateBundleResponse() *client.BundlePublicV2Model {
 	integrationData.ResourcesScopes.SetTo([]client.ResourcesScopeIntegrationAccessTargetPublicV2Model{resourceScope})
 	integrationTarget.Integration.SetTo(integrationData)
 
-	// Add an access scope target
 	accessScopeTarget := client.AccessBundleAccessTargetPublicV2Model{}
 	accessScopeData := client.AccessScopeAccessTargetPublicV2Model{
 		AccessScopeID:   "scope-123",
