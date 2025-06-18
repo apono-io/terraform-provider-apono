d3b5f067a3fc6529a98eaac81355c38b0b030754
Tomer Heber <heber.tomer@gmail.com>
2025-06-18 11:30:47 -0500

    Add acceptence test for the not found use-case (#151)

--- internal/v2/resources/apono_resource_integration_resource_test.go
+++ internal/v2/resources/apono_resource_integration_resource_test.go
@@ -4,26 +4,26 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/apono-io/terraform-provider-apono/internal/v2/api/client"
 	"github.com/apono-io/terraform-provider-apono/internal/v2/common"
 	"github.com/apono-io/terraform-provider-apono/internal/v2/testcommon"
 	"github.com/apono-io/terraform-provider-apono/internal/v2/testcommon/testprovider"
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
-func TestAccAponoResourceIntegrationResource(t *testing.T) {
-	rName := acctest.RandomWithPrefix("tf-acc-test")
-	resourceName := "apono_resource_integration.test"
-	integrationType := common.MockDuck
+func TestAccAponoResourceIntegration(t *testing.T) {
+	t.Run("ResourceIntegration", func(t *testing.T) {
+		testAccAponoResourceIntegrationResource(t)
+	})
+	t.Run("ResourceIntegrationDrift", func(t *testing.T) {
+		testAccAponoResourceIntegrationResourceDrift(t)
+	})
+}
 
-	connectorID := testcommon.GetTestConnectorID(t)
-
-	resourceType := common.MockDuck
-	customAccessDetails := "Example access instructions"
-	updatedCustomAccessDetails := "Updated access instructions"
-
-	testAccAponoResourceIntegrationConfig := func(name, integrationType, connectorID, resourceType, customAccessDetails string) string {
-		return fmt.Sprintf(`
+func testAccAponoResourceIntegrationConfig(name, connectorID, customAccessDetails string) string {
+	return fmt.Sprintf(`
 resource "apono_resource_integration" "test" {
   name                    = "%s"
   type                    = "%s"
@@ -41,27 +41,36 @@ resource "apono_resource_integration" "test" {
     }
   }
 }
-`, name, integrationType, connectorID, resourceType, customAccessDetails)
-	}
+`, name, common.MockDuck, connectorID, common.MockDuck, customAccessDetails)
+}
+
+func testAccAponoResourceIntegrationResource(t *testing.T) {
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	resourceName := "apono_resource_integration.test"
+
+	connectorID := testcommon.GetTestConnectorID(t)
+
+	customAccessDetails := "Example access instructions"
+	updatedCustomAccessDetails := "Updated access instructions"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testcommon.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: testprovider.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAponoResourceIntegrationConfig(rName, integrationType, connectorID, resourceType, customAccessDetails),
+				Config: testAccAponoResourceIntegrationConfig(rName, connectorID, customAccessDetails),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(resourceName, "id"),
 					resource.TestCheckResourceAttr(resourceName, "name", rName),
-					resource.TestCheckResourceAttr(resourceName, "type", integrationType),
+					resource.TestCheckResourceAttr(resourceName, "type", common.MockDuck),
 					resource.TestCheckResourceAttr(resourceName, "connector_id", connectorID),
-					resource.TestCheckResourceAttr(resourceName, "connected_resource_types.0", resourceType),
+					resource.TestCheckResourceAttr(resourceName, "connected_resource_types.0", common.MockDuck),
 					resource.TestCheckResourceAttr(resourceName, "custom_access_details", customAccessDetails),
 					resource.TestCheckResourceAttr(resourceName, "integration_config.key", "value"),
 				),
 			},
 			{
-				Config: testAccAponoResourceIntegrationConfig(rName, integrationType, connectorID, resourceType, updatedCustomAccessDetails),
+				Config: testAccAponoResourceIntegrationConfig(rName, connectorID, updatedCustomAccessDetails),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(resourceName, "id"),
 					resource.TestCheckResourceAttr(resourceName, "name", rName),
@@ -75,4 +84,80 @@ resource "apono_resource_integration" "test" {
 			},
 		},
 	})
+}
+
+func testAccAponoResourceIntegrationResourceDrift(t *testing.T) {
+	rName := acctest.RandomWithPrefix("tf-acc-test-drift")
+	resourceName := "apono_resource_integration.test"
+
+	connectorID := testcommon.GetTestConnectorID(t)
+
+	customAccessDetails := "Example access instructions"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testcommon.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: testprovider.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAponoResourceIntegrationConfig(rName, connectorID, customAccessDetails),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "type", common.MockDuck),
+					resource.TestCheckResourceAttr(resourceName, "connector_id", connectorID),
+					resource.TestCheckResourceAttr(resourceName, "connected_resource_types.0", common.MockDuck),
+					resource.TestCheckResourceAttr(resourceName, "custom_access_details", customAccessDetails),
+					resource.TestCheckResourceAttr(resourceName, "integration_config.key", "value"),
+				),
+			},
+			// Delete the resource via API to simulate drift
+			{
+				Config: testAccAponoResourceIntegrationConfig(rName, connectorID, customAccessDetails),
+				Check: resource.ComposeTestCheckFunc(
+					testAccDeleteResourceIntegrationViaAPI(t, resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+			// Apply again to recreate the resource
+			{
+				Config: testAccAponoResourceIntegrationConfig(rName, connectorID, customAccessDetails),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "type", common.MockDuck),
+					resource.TestCheckResourceAttr(resourceName, "connector_id", connectorID),
+					resource.TestCheckResourceAttr(resourceName, "connected_resource_types.0", common.MockDuck),
+					resource.TestCheckResourceAttr(resourceName, "custom_access_details", customAccessDetails),
+					resource.TestCheckResourceAttr(resourceName, "integration_config.key", "value"),
+				),
+			},
+		},
+	})
+}
+
+// testAccDeleteResourceIntegrationViaAPI is a test helper that deletes the resource integration via API.
+func testAccDeleteResourceIntegrationViaAPI(t *testing.T, resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("resource %s not found", resourceName)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("resource %s has no ID set", resourceName)
+		}
+
+		// Get the API client from the provider
+		clientInvoker := testcommon.GetTestClient(t)
+
+		// Delete the integration via API
+		err := clientInvoker.DeleteIntegrationV4(t.Context(), client.DeleteIntegrationV4Params{
+			ID: rs.Primary.ID,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to delete integration via API: %v", err)
+		}
+
+		return nil
+	}
 }
